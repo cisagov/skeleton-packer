341d6e1c140ebcc860b212715b19d9d0c6f8063c
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2023-02-02 10:23:00 -0500

    Use a testing branch of cisagov/ami-build-iam-user-tf-module

--- terraform-build-user/main.tf
+++ terraform-build-user/main.tf
@@ -1,5 +1,5 @@
 module "iam_user" {
-  source = "github.com/cisagov/ami-build-iam-user-tf-module"
+  source = "github.com/cisagov/ami-build-iam-user-tf-module?ref=improvement%2Fsupport_build_users_with_no_ssm_needs"
 
   providers = {
     aws                       = aws
