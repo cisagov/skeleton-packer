87e1fd1f15ad030557e460e26b001d4b38391d34
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-09-25 12:24:33 -0400

    Move calculation of recent AMIs to its own file

--- terraform-post-packer/main.tf
+++ terraform-post-packer/main.tf
@@ -41,14 +41,6 @@ moved {
   to   = module.ami_launch_permission_x86_64
 }
 
-locals {
-  # Really we only want the var.recent_ami_count most recent AMIs, but
-  # we have to cover the case where there are fewer than that many
-  # AMIs in existence.  Hence the min()/length() tomfoolery.
-  recent_amis_arm64  = toset(slice(data.aws_ami_ids.historical_amis_arm64.ids, 0, min(var.recent_ami_count, length(data.aws_ami_ids.historical_amis_arm64.ids))))
-  recent_amis_x86_64 = toset(slice(data.aws_ami_ids.historical_amis_x86_64.ids, 0, min(var.recent_ami_count, length(data.aws_ami_ids.historical_amis_x86_64.ids))))
-}
-
 # Deregister any AMIs outside of local.recent_amis_x86_64 and
 # local.recent_amis_x86_64, but first verify that they no longer have
 # launch permissions associated with them.
