995c8e30e7e2101e63c527cc636bde5cd488429f
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-09-25 12:06:31 -0400

    Add a janitor resource to clean up old AMIs
    This resource goes through any existing AMIs that aren't included in
    the list of recent AMIs, checks if the AMI has any launch permissions
    attached (it shouldn't).  If no launch permissions are present then
    the AMI is deregistered and the underlying EBS snapshot is deleted.

--- terraform-post-packer/main.tf
+++ terraform-post-packer/main.tf
@@ -30,10 +30,7 @@ data "aws_ami_ids" "historical_amis_arm64" {
 
 # Assign launch permissions to the ARM64 AMIs
 module "ami_launch_permission_arm64" {
-  # Really we only want the var.recent_ami_count most recent AMIs, but
-  # we have to cover the case where there are fewer than that many
-  # AMIs in existence.  Hence the min()/length() tomfoolery.
-  for_each = toset(slice(data.aws_ami_ids.historical_amis_arm64.ids, 0, min(var.recent_ami_count, length(data.aws_ami_ids.historical_amis_arm64.ids))))
+  for_each = local.recent_amis_arm64
 
   source = "github.com/cisagov/ami-launch-permission-tf-module"
 
@@ -85,10 +82,7 @@ moved {
 
 # Assign launch permissions to the x86-64 AMIs
 module "ami_launch_permission_x86_64" {
-  # Really we only want the var.recent_ami_count most recent AMIs, but
-  # we have to cover the case where there are fewer than that many
-  # AMIs in existence.  Hence the min()/length() tomfoolery.
-  for_each = toset(slice(data.aws_ami_ids.historical_amis_x86_64.ids, 0, min(var.recent_ami_count, length(data.aws_ami_ids.historical_amis_x86_64.ids))))
+  for_each = local.recent_amis_x86_64
 
   source = "github.com/cisagov/ami-launch-permission-tf-module"
 
@@ -111,4 +105,51 @@ module "ami_launch_permission_x86_64" {
 moved {
   from = module.ami_launch_permission
   to   = module.ami_launch_permission_x86_64
+}
+
+locals {
+  # Really we only want the var.recent_ami_count most recent AMIs, but
+  # we have to cover the case where there are fewer than that many
+  # AMIs in existence.  Hence the min()/length() tomfoolery.
+  recent_amis_arm64  = toset(slice(data.aws_ami_ids.historical_amis_arm64.ids, 0, min(var.recent_ami_count, length(data.aws_ami_ids.historical_amis_arm64.ids))))
+  recent_amis_x86_64 = toset(slice(data.aws_ami_ids.historical_amis_x86_64.ids, 0, min(var.recent_ami_count, length(data.aws_ami_ids.historical_amis_x86_64.ids))))
+}
+
+# Deregister any AMIs outside of local.recent_amis_x86_64 and
+# local.recent_amis_x86_64, but first verify that they no longer have
+# launch permissions associated with them.
+resource "terraform_data" "ami_janitor" {
+  for_each = setunion(setsubtract(toset(data.aws_ami_ids.historical_amis_arm64.ids), local.recent_amis_arm64), setsubtract(toset(data.aws_ami_ids.historical_amis_x86_64.ids), local.recent_amis_x86_64))
+
+  triggers_replace = {
+    # This forces the provisioner to run every time this Terraform
+    # code is run.
+    always_run = timestamp()
+    ami_id     = each.value
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOT
+      echo "Starting AMI launch permission validation check..."
+
+      export AWS_PROFILE=cool-images-ec2amicreate
+      export AWS_REGION=us-east-1
+
+      # Extract launch permissions
+      permissions=$(aws ec2 describe-image-attribute --image-id "${self.triggers_replace.ami_id}" --attribute launchPermission --query 'LaunchPermissions[*]' --output text)
+
+      # If no external accounts are attached, drop the AMI and its
+      # snapshots in one go.
+      if [ -z "$permissions" ] || [ "$permissions" == "None" ]; then
+        echo "Deregistering AMI ${self.triggers_replace.ami_id} and purging backing snapshots because no launch permissions are attached."
+
+        # Note that the AWS CLI option --delete-associated-snapshots
+        # handles snapshot deletion automatically.
+        aws ec2 deregister-image --image-id "${self.triggers_replace.ami_id}" --delete-associated-snapshots
+      else
+        echo "Skipping AMI ${self.triggers_replace.ami_id} because active launch permissions were found."
+      fi
+    EOT
+  }
 }
--- terraform-post-packer/versions.tf
+++ terraform-post-packer/versions.tf
@@ -1,7 +1,10 @@
 terraform {
   # Version 1.1 of Terraform is the first version to support the
   # nullable key in variable definitions.
-  required_version = "~> 1.1"
+  #
+  # Version 1.5 of Terraform is the first version to support the
+  # terraform_data resource type.
+  required_version = "~> 1.5"
 
   # If you use any other providers you should also pin them to the
   # major version currently being used.  This practice will help us
