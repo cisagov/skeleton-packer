2e5f88f55abd7b858de7636f24bdfcbc241241ff
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-09-25 12:22:29 -0400

    Move historical AMIs jazz to its own file

--- terraform-post-packer/main.tf
+++ terraform-post-packer/main.tf
@@ -1,33 +1,3 @@
-# Use aws_caller_identity with the default provider (Images account)
-# so we can provide the Images account ID below
-data "aws_caller_identity" "images" {
-}
-
-# The IDs of all ARM64 cisagov/skeleton-packer AMIs
-data "aws_ami_ids" "historical_amis_arm64" {
-  owners = [data.aws_caller_identity.images.account_id]
-
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["example-hvm-*-arm64-ebs"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 # Assign launch permissions to the ARM64 AMIs
 module "ami_launch_permission_arm64" {
   for_each = local.recent_amis_arm64
@@ -42,42 +12,6 @@ module "ami_launch_permission_arm64" {
   account_name_regex   = var.ami_share_account_name_regex
   ami_id               = each.value
   extraorg_account_ids = var.extraorg_account_ids
-}
-
-# The IDs of all x86-64 cisagov/skeleton-packer AMIs
-data "aws_ami_ids" "historical_amis_x86_64" {
-  owners = [data.aws_caller_identity.images.account_id]
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["example-hvm-*-x86_64-ebs"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-# This moved block allows us to rename the resources at
-# aws_ami_ids.historical_amis to aws_ami_ids.historical_amis_x86_64
-# instead of destroying and recreating them with a new name.
-#
-# TODO: Consider removing this moved block when it is no longer
-# needed.  See cisagov/skeleton-packer#369 for more details.
-moved {
-  from = aws_ami_ids.historical_amis
-  to   = aws_ami_ids.historical_amis_x86_64
 }
 
 # Assign launch permissions to the x86-64 AMIs
